898b0e802d92871d622fee676cfd4b87ef95f025
usman-tahir <tahir.usman.ali94@outlook.com>
2017-05-28 19:41:39 -0400

    improved the array implementation by using an ArrayList

--- software-construction/java-snippets/HailstoneArray.java
+++ software-construction/java-snippets/HailstoneArray.java
@@ -1,11 +1,11 @@
+import java.util.ArrayList;
 
 public class HailstoneArray {
   public static int[] generateHailstoneSequence(int start) {
-    int[] sequence = new int[100]; // Not advised due to memory constraint
-    int i = 0;
+    List<Integer> sequence = new ArrayList<Integer>();
 
     while (start != 1) {
-      sequence[i] = start;
+      sequence.add(start);
       i = i + 1;
 
       if (start % 2 == 0) {
@@ -14,8 +14,7 @@ public class HailstoneArray {
         start = start * 3 + 1;
       }
     }
-    sequence[i] = start;
-    i = i + 1;
+    sequence.add(start);
 
     return sequence;
   }
